e8c5fd6c491a582395448fe267c5a69b8329240c
Varun0157 <varun.edachali@gmail.com>
2024-11-15 12:31:59 +0530

    [feat] custom quantize tensor

--- src/model.py
+++ src/model.py
@@ -6,7 +6,7 @@ from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 
 
 class GPTNeo(nn.Module):
-    def __init__(self, models_path: str, device):
+    def __init__(self, models_path: str, device: torch.device):
         super().__init__()
         self.device = device
 
@@ -14,12 +14,14 @@ class GPTNeo(nn.Module):
         mod_path = os.path.join(models_path, "gpt-neo_model")
         self.tokenizer = GPT2Tokenizer.from_pretrained(tok_path)
         self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.model = GPTNeoForCausalLM.from_pretrained(mod_path).to(device)
+
+        self.model = GPTNeoForCausalLM.from_pretrained(mod_path)
+        self.model.to(self.device)  # type: ignore
         self.model.eval()
 
     def quantize(self):
         self.model = torch.quantization.quantize_dynamic(
-            self.model, {torch.nn.Linear}, dtype=torch.int8
+            self.model, {torch.nn.Linear}, dtype=torch.qint8
         )
 
     def forward(self, prompt: str):
@@ -31,3 +33,15 @@ class GPTNeo(nn.Module):
 
         outputs = self.model(input_ids, attention_mask=attention_mask, labels=input_ids)
         return outputs
+
+    @staticmethod
+    def quantize_tensor(tensor: torch.Tensor, dtype: torch.dtype) -> torch.Tensor:
+        NEW_MIN, NEW_MAX = torch.iinfo(dtype).min, torch.iinfo(dtype).max
+        OLD_MIN, OLD_MAX = tensor.min(), tensor.max()
+
+        VAL_RANGE = NEW_MAX - NEW_MIN
+        scale = (OLD_MAX - OLD_MIN) / VAL_RANGE
+        zero_point = OLD_MIN
+
+        quantized = torch.quantize_per_tensor(tensor, scale, zero_point, dtype)
+        return quantized
