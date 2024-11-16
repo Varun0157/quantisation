e6bebd0c2c86d9c2ecd6d50ee75ad9a177e3d772
Varun0157 <varun.edachali@gmail.com>
2024-11-16 14:07:06 +0530

    [feat] custom quant with cuda

--- src/from_scratch.py
+++ src/from_scratch.py
@@ -13,8 +13,10 @@ class Qint_Linear(nn.Module):
         bias: bool = True,
         dtype: torch.dtype = torch.float32,
         goal_dtype: torch.dtype = torch.int8,
+        device: torch.device = torch.device("cpu"),
     ):
         super().__init__()
+        self.device = device
 
         self.in_dtype = dtype
         assert goal_dtype in [torch.int8, torch.int16, torch.int32, torch.int64]
@@ -24,7 +26,6 @@ class Qint_Linear(nn.Module):
             torch.iinfo(goal_dtype).min,
             torch.iinfo(goal_dtype).max,
         )
-        print(self.NEW_MIN, self.NEW_MAX)
         self.register_buffer(
             "weights",
             torch.randint(
@@ -32,12 +33,16 @@ class Qint_Linear(nn.Module):
                 self.NEW_MAX,
                 (out_features, in_features),
                 dtype=goal_dtype,
+                device=device,
             ),
         )
-        self.register_buffer("scales", torch.randn((out_features), dtype=dtype))
-
+        self.register_buffer(
+            "scales", torch.randn((out_features), dtype=dtype, device=self.device)
+        )
         if bias:
-            self.register_buffer("bias", torch.randn((1, out_features), dtype=dtype))
+            self.register_buffer(
+                "bias", torch.randn((1, out_features), dtype=dtype, device=self.device)
+            )
         else:
             self.bias = None
 
@@ -45,10 +50,10 @@ class Qint_Linear(nn.Module):
         weights_in = weights.clone().to(self.in_dtype)
         scales = weights_in.abs().max(dim=1).values / self.NEW_MAX
         scales = scales.to(weights.dtype)
-        self.scales = scales
+        self.scales = scales.to(self.device)
 
         quantized_weights = torch.round(weights / scales[:, None]).to(self.out_dtype)
-        self.weights = quantized_weights
+        self.weights = quantized_weights.to(self.device)
 
     def forward(self, x: torch.Tensor):
         converted_weights = self.weights.to(x.dtype)
@@ -61,25 +66,28 @@ class Qint_Linear(nn.Module):
 
 
 def quantize_linear_layers(
-    module: AutoModelForCausalLM, goal_dtype: torch.dtype = torch.int8
+    module: AutoModelForCausalLM,
+    goal_dtype: torch.dtype,
+    device: torch.device = torch.device("cpu"),
 ):
     for name, child in module.named_children():  # type: ignore
         if not isinstance(child, nn.Linear):
-            quantize_linear_layers(child, goal_dtype)
+            quantize_linear_layers(child, goal_dtype, device)
             continue
 
         old_bias, old_weights = child.bias, child.weight
-        bias = old_bias is not None
+        has_bias = old_bias is not None
         quantized_layer = Qint_Linear(
             child.in_features,
             child.out_features,
-            bias,
+            has_bias,
             child.weight.dtype,
             goal_dtype,
+            device,
         )
 
         setattr(module, name, quantized_layer)
 
         getattr(module, name).quantize(old_weights)
-        if bias:
+        if has_bias:
             getattr(module, name).bias = old_bias
